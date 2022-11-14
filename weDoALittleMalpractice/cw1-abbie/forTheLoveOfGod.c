7800c9e76f9f024f3cb7c767cc8bdb6cb3ef8921
Louis Bishop <louis.bishop@live.co.uk>
2022-11-14 17:43:47 +0000

    yea

--- weDoALittleMalpractice/cw1-abbie/forTheLoveOfGod.c
+++ weDoALittleMalpractice/cw1-abbie/forTheLoveOfGod.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 #include <math.h>
 
-int roundNo(float); //header for custom round function
+int roundNo(float);
 
-int main( int argc, char **argv )
-{
+int main( int argc, char **argv ){
 	FILE *fp;
 	// command line arguments
-	if(argc != 2)
-	{
+	if(argc != 2){
   		printf("No input file name given. Exiting.\n");
 		return 0;
 	}
-	//verify the specified input file is accessible
-	if(!(fopen(argv[1],"r")))
-	{
+	if(!(fopen(argv[1],"r"))){
   		printf("Input file does not exist. Exiting.\n");
 		return 0;
   	}
@@ -43,20 +39,17 @@ int main( int argc, char **argv )
  			printf("Found an invalid student id: %d. Exiting.\n",id); // requires student id
 			return 0;
 		}
-		//verify each grade is valid and correct where needed
 		for(j = 1; j < 5; j++){
 			int grade = results[i][j];
 			if((grade > 100 || grade < 1)){
   				printf("Found an invalid grade: id %d grade %d. Exiting.\n", id,grade ); // requires student id and relevant grade
 				return 0;
 			}
-
 			if(grade<20){	
 				results[i][j] = 20;
 				printf("Correcting student %d grade %d\n",id,grade); // requires student id and relevant grade
 			}
-			if(grade>90)
-			{
+			if(grade>90){
 				results[i][j] = 90;
 				printf("Correcting student %d grade %d\n",id,grade); // requires student id and relevant grade
 			}
@@ -64,9 +57,7 @@ int main( int argc, char **argv )
 	}
   	// compute averages
  	printf("Computing averages.\n");
-	//compute average grade for each student
-	for(i = 0; i < 32; i++)
-	{
+	for(i = 0; i < 32; i++){
 		int average = roundNo(((float)results[i][1]+(float)results[i][2]+(float)results[i][3]+(float)results[i][4])/4);
 		results[i][5] = average;
 	}
@@ -75,19 +66,14 @@ int main( int argc, char **argv )
  	printf("Output file. Opening.\n");
 	
 	fp = fopen("averages.txt","w");
-	//write the student ids and associated averages to the output file
-	for(i = 0; i < 32; i++)
-	{
+	for(i = 0; i < 32; i++){
 		fprintf(fp," %d %d\n",results[i][0],results[i][5]);
 	}
-
 	printf("Output file. Closing.\n");
-	fclose(fp);
-	
+	fclose(fp);	
  	return 0;
 }
 //custom round function
-int roundNo(float num)
-{
+int roundNo(float num){
 	return (int) (num < 0 ? num - 0.5 : num + 0.5);
 }
