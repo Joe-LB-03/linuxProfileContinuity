02b9956a4b243369e589ad03f643cd22cd2e5b8b
Louis Bishop <louis.bishop@live.co.uk>
2022-11-21 18:01:45 +0000

    ws9

---
+++ Week8Programs/worksheet9/add.c
@@ -0,0 +1,61 @@
+
+#include "linkedList.h"
+#include "add.h"
+
+/*
+ * add a node to the list at the head
+ */
+
+Node *addNodeAtHead( Node *head, Node *newNode ) {
+
+  newNode->next = head;
+
+  return newNode;
+}
+
+/*
+ * add a node to the list at the tail
+ */
+
+Node *addNodeAtTail( Node *head, Node *newNode )
+{
+	newNode->next = NULL;
+	Node *last = head;
+
+	if(head == NULL)
+	{
+		head = newNode;
+		return head;
+	}
+	while(last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = newNode;
+    return head;
+} 
+
+/*
+ * insert a node after a given location
+ */
+
+void insertNodeAfter( Node *location, Node *new ) {
+
+  new->next = location->next;
+  location->next = new;
+
+  return;
+}
+
+/*
+ * delete a node from the list
+ */
+
+Node *deleteNextNode( Node *location ) {
+
+  Node *toBeRemoved = location->next;
+  location->next = toBeRemoved->next;  // unlink from the list
+
+  return toBeRemoved;
+}
+
---
+++ Week8Programs/worksheet9/add.h
@@ -0,0 +1,6 @@
+
+Node *addNodeAtHead( Node *head, Node *new );
+Node *addNodeAtTail( Node *head, Node *new);
+void insertNodeAfter( Node *location, Node *new );
+Node *deleteNextNode( Node *location );
+
