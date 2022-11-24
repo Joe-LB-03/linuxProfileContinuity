5cfca804384fa96e188ae415c6f41e6b6a590749
Louis Bishop <louis.bishop@live.co.uk>
2022-11-24 02:22:18 +0000

    dynamic 2d array in a struct. bitchin.

--- coursework2/initGame.c
+++ coursework2/initGame.c
@@ -13,16 +13,15 @@ Game *initGame( int boardSize, int winLength )
 {
     Game *game;
     int i,j;
-
     // for incorrect boardSize or winLength you should return a NULL pointer
     if(boardSize < 3 || boardSize > 8 || winLength < 3 || winLength > boardSize)
     {
         printf("Incorrect parameter values for board size or win length. Exiting\n");
         return NULL;
     }
-
     // allocate the Game data structure
-    game = (Game *)malloc(sizeof(Game));
+    game = (Game *)malloc(sizeof(Game) + (sizeof(char)*boardSize*boardSize));
+    createArray(game);
 
     // intialise the Game data structure values 
     game->boardSize = boardSize;
@@ -34,9 +33,20 @@ Game *initGame( int boardSize, int winLength )
         for(j=0;j<boardSize;j++)
         {
             game->board[i][j] = '.';
+            
         }
     }
     return game;
 }
 
+void createArray(Game *game)
+{
+    int i;
+    char* array[game->boardSize];
+    for(i=0;i<game->boardSize;i++)
+    {
+        array[i] = (char*)malloc(sizeof(char)*game->winLength);   
+    }
 
+    game->board = array;
+}
